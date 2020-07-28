f3c5c74ab71249688dc83daa01ebc883e871513b
Rbeltran37 <Rbeltran37@gmail.com>
2020-07-28 18:12:17 -0400

    implemented VR with oculus integration

---
+++ ReGameCognitive/Assets/Scripts/loadMainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadMainMenu : MonoBehaviour
+{
+
+    private void OnTriggerEnter()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+}
